a6374bb65b509c49402a5f9b442053c21e0318ce
Lucas Mendes Vieira <lmvieiratv@gmail.com>
2024-02-26 22:53:25 -0300

    Tirando comentarios desnecessários

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 import board.Board;
 import game.Game;
 import java.util.Scanner;
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //Imports
+        
         Game game = new Game();
         Board board = game.board();
         Scanner scanner = new Scanner(System.in);
